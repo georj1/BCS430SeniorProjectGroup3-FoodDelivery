6a892c883eafb379bcd114b55ba87169c6cee508
Moaath Alrajab <moaath@alrajab.net>
2019-03-21 22:22:32 -0400

    Add homework 9 requirements. All ToDos to be completed.

--- classesInheritance/src/classesInheritance/Person.java
+++ classesInheritance/src/classesInheritance/Person.java
@@ -6,7 +6,7 @@ public abstract class Person {
 	private String address;
 	private short age;
 	
-	
+	// constructor that takes only two paras
 	public Person(String name, short age) {
 		super();
 		this.name = name;
@@ -16,19 +16,22 @@ public abstract class Person {
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public abstract String getAddress() ;
+	
 	public abstract void setAddress(String address);
+	
 	
 	public short getAge() {
 		return age;
 	}
+	
 	public void setAge(short age) {
 		this.age = age;
 	}
 	
-	
-
 }
--- classesInheritance/src/classesInheritance/Student.java
+++ classesInheritance/src/classesInheritance/Student.java
@@ -1,6 +1,12 @@
 package classesInheritance;
 
 public class Student {
+	// ToDo 1: Make this class a child of Person
 	
+	// ToDo 2: Fix the resulting errors
+	
+	// ToDo 3: Add a field for GPA and create setter and getter
+	
+	// ToDo 4: Add comments to your code
 
 }
