5c78fd69671b0b161a9f94b6660f5dcb7d13e98e
ChristopherSisa <70668170+ChristopherSisa@users.noreply.github.com>
2020-11-09 21:02:57 -0500

    Update Student.java

--- classesInheritance/src/classesInheritance/Student.java
+++ classesInheritance/src/classesInheritance/Student.java
@@ -1,12 +1,19 @@
 package classesInheritance;
 
-public class Student {
-	// ToDo 1: Make this class a child of Person
+public class Student extends Person {// makes student a child of person
+
 	
-	// ToDo 2: Fix the resulting errors
+	// ToDo 3: Add a field for GPA 
+	private double GPA;
 	
-	// ToDo 3: Add a field for GPA and create setter and getter
-	
-	// ToDo 4: Add comments to your code
+	// getter
+	public void setGPA(double GPA){
+		this.GPA = GPA;
+	}
+	//getter
+	public double getGPA(){
+		return GPA;
+	}
+
 
 }
