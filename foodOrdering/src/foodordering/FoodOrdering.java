4351b72359a92b094af358a871cd8daef282fec1
georj1 <georj1@farmingdale.edu>
2021-10-10 21:12:04 -0400

    Changes to FoodOrdering to make room for Customer
    Added the ability to get user data about Customers, added the
    functionality to allow data to be pulled from database, added a TODO:
    for what still needs to be done -Jack

--- foodOrdering/src/foodordering/FoodOrdering.java
+++ foodOrdering/src/foodordering/FoodOrdering.java
@@ -28,12 +28,12 @@ public class FoodOrdering {
     	Scanner scin = new Scanner(System.in); //Scanner is used to get data from the user -Jack
     	
     	String sql=""; //right now this is an empty string but it will contain the actual SQL statements and then be passed to the execute update method -Jack
-    	System.out.println("Are you a Restaurant or a Customer?: "); //For now this well be used to see if a Restaurant or Customer is being added -Jack
+    	System.out.println("Please let us know if you are a Restaurant or Customer and if you owuld like to enter or see data: "); //For now this well be used to see if a Restaurant or Customer is being added -Jack
     	String rOrC = scin.nextLine(); //code to put the user input into a String -Jack
-    	if(rOrC=="Restaurant" || rOrC=="R") //checking if they input Restaurant or R for shorthand testing -Jack
+    	if(rOrC=="Restaurant Enter" || rOrC=="R E") //checking if they input Restaurant or R for shorthand testing -Jack
     	{
     		Restaurant r1 = new Restaurant(); //create a new Restaurant object that the user will enter information into, will eventually be sent to database -Jack
-    		System.out.println("Enter Restaurant Name: "); //very repetitive, will be done better later, all of these will just get the information to fill out Restaurant -Jack
+    		System.out.println("\nEnter Restaurant Name: "); //very repetitive, will be done better later, all of these will just get the information to fill out Restaurant -Jack
     		r1.setRestaurantName(scin.nextLine()); //set the RestaurantName of our temporary Restaurant to what they enter, could be error checked later -Jack
     		System.out.println("\nEnter Restaurant City: "); 
     		r1.setRestaurantCity(scin.nextLine()); 
@@ -45,10 +45,39 @@ public class FoodOrdering {
     		r1.setRestaurantZip(scin.nextLine());
     		System.out.println("\nEnter Restaurant Type: "); 
     		r1.setRestaurantType(scin.nextLine());
-    	}
-    	else
-    	{
     		
+    		//TODO: input Restaurant data into database - Jack
+    	}
+    	else if(rOrC=="Customer Enter" || rOrC=="C E")
+    	{
+    		Customer c1 = new Customer(); //create a new Customer object that the user will enter information into, will eventually be sent to database -Jack
+    		System.out.println("\nEnter First Name: ");
+    		c1.setCustomerFName(scin.nextLine());
+    		System.out.println("\nEnter Last Name: ");
+    		c1.setCustomerLName(scin.nextLine());
+    		System.out.println("\nEnter Email: ");
+    		c1.setCustomerEmail(scin.nextLine());
+    		System.out.println("\nEnter Phone Number: ");
+    		c1.setCustomerPhone(scin.nextLine());
+    		System.out.println("\nEnter City: ");
+    		c1.setCustomerCity(scin.nextLine());
+    		System.out.println("\nEnter State: ");
+    		c1.setCustomerState(scin.nextLine());
+    		System.out.println("\nEnter Street: ");
+    		c1.setCustomerStreet(scin.nextLine());
+    		System.out.println("\nEnter Zip: ");
+    		c1.setCustomerZip(scin.nextLine());
+    		//Same as Restaurant code -Jack
+    		
+    		//TODO: input Customer data into database - Jack
+    	}
+    	else if(rOrC=="Restaurant See" || rOrC=="R S")
+    	{
+    		//TODO: Pull Restaurant data from database and display it to user -Jack
+    	}
+    	else if(rOrC=="Customer See" || rOrC=="C S")
+    	{
+    		//TODO: Pull Customer data from database and display it to user -Jack
     	}
     	try {
     	Connection connection = DriverManager.getConnection(url); //this is attempting to open the connection to the server -Jack
