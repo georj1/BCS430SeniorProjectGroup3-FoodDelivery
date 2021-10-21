815dcf1b36df1f133fb065d5e24710d4d7223181
georj1 <georj1@farmingdale.edu>
2021-10-20 22:11:48 -0400

    Changed If statement to Switch, added a select restaurant method
    Still need some fixes so not great yet, I changed our navigation to a
    Switch that should make navigation easier until we get Java FX up, also
    created the select restaurant method just still need to add the menu and
    Food item table and have that all display.

--- foodOrdering/src/foodordering/FoodOrdering.java
+++ foodOrdering/src/foodordering/FoodOrdering.java
@@ -27,7 +27,7 @@ public class FoodOrdering {
     	String user ="NT Service\\MSSQL$SQLEXPRESS"; //not needed right now but might be needed for remote access -Jack
     	String password="bcs430group3"; // same as above comment -Jack
     	Scanner scin = new Scanner(System.in); //Scanner is used to get data from the user -Jack
-    	String userIn="";
+    	int userIn=0;
     	 //right now this is an empty string but it will contain the actual SQL statements and then be passed to the execute update method -Jack
     	
     	
@@ -36,30 +36,47 @@ public class FoodOrdering {
     		//Test
     		Connection connection = DriverManager.getConnection(url); //this is attempting to open the connection to the server -Jack
     		System.out.println("Connected"); //Display a message to show it successfully connected -Jack
-    		while(!userIn.equals("STOP")) //Once again is temporary, for now this is so it can be tested and accessed multiple times, will eventually have a nicer UI -Jack
+    		while(userIn !=0) //Once again is temporary, for now this is so it can be tested and accessed multiple times, will eventually have a nicer UI -Jack
     		{
-    			System.out.println("Please let us know what you'd like to do:  "); //For now this well be used to see if a Restaurant or Customer is being added and other functionality -Jack
-    			userIn = scin.nextLine(); //code to put the user input into a String -Jack
-    			if(userIn.equals("Restaurant Enter") || userIn.equals("R E")) //checking if they input Restaurant or R for shorthand testing -Jack
+    			System.out.println("Please let us know what you'd like to do by entering the number:  "
+    					+ "/n[1] Enter a Restaurant"
+    					+"/n[2] View Restaurants"
+    					+ "/n[3] Enter a Customer"
+    					+ "/n[4] View Customers"
+    					+ "/n[5] Select a Restaurant"
+    					+ "/n[6] Search Restaurnts by Zip Code"
+    					+ "/n[0] Exit"); //For now this well be used to see if a Restaurant or Customer is being added and other functionality -Jack
+    			userIn = scin.nextInt(); //code to put the user input into a String -Jack
+    			switch(userIn)
     			{
+    			case 0: 
+    				break;
+    			case 1: 
     				Restaurant r1 = new Restaurant(); //create a new Restaurant object that the user will enter information into, will eventually be sent to database -Jack
-    				System.out.println("\nEnter Restaurant Name: "); //very repetitive, will be done better later, all of these will just get the information to fill out Restaurant -Jack
-    				r1.setRestaurantName(scin.nextLine()); //set the RestaurantName of our temporary Restaurant to what they enter, could be error checked later -Jack
-    				System.out.println("\nEnter Restaurant City: "); 
-    				r1.setRestaurantCity(scin.nextLine()); 
-    				System.out.println("\nEnter Restaurant State: "); 
-    				r1.setRestaurantState(scin.nextLine());
-    				System.out.println("\nEnter Restaurant Street: "); 
-    				r1.setRestaurantStreet(scin.nextLine());
-    				System.out.println("\nEnter Restaurant Zip: "); 
-    				r1.setRestaurantZip(scin.nextLine());
-    				System.out.println("\nEnter Restaurant Type: "); 
-    				r1.setRestaurantType(scin.nextLine());
-    				
-    				insertRestaurant(r1, connection); //Call the method that inserts the data -Jack
-    			}
-    			else if(userIn.equals("Customer Enter") || userIn.equals("C E"))
-    			{
+				    System.out.println("\nEnter Restaurant Name: "); //very repetitive, will be done better later, all of these will just get the information to fill out Restaurant -Jack
+				    r1.setRestaurantName(scin.nextLine()); //set the RestaurantName of our temporary Restaurant to what they enter, could be error checked later -Jack
+				    System.out.println("\nEnter Restaurant City: "); 
+				    r1.setRestaurantCity(scin.nextLine()); 
+				    System.out.println("\nEnter Restaurant State: "); 
+				    r1.setRestaurantState(scin.nextLine());
+				    System.out.println("\nEnter Restaurant Street: "); 
+				    r1.setRestaurantStreet(scin.nextLine());
+				    System.out.println("\nEnter Restaurant Zip: "); 
+				    r1.setRestaurantZip(scin.nextLine());
+				    System.out.println("\nEnter Restaurant Type: "); 
+				    r1.setRestaurantType(scin.nextLine());
+				
+				    insertRestaurant(r1, connection); 
+    				break;
+    			case 2:
+    				showRestaurants(connection); //Call the method that shows the data -Ahsan
+    				System.out.println("Enter the nummber of the restaurant you want to order from: ");
+    				Scanner selectR = new Scanner(System.in); //Scanner is used to get data from the user -Jack
+    		    	int rSelectIn=0;
+    		    	rSelectIn=selectR.nextInt();
+    		    	selectRestaurant(connection, rSelectIn);
+    		    	break;
+    			case 3:
     				Customer c1 = new Customer(); //create a new Customer object that the user will enter information into, will eventually be sent to database -Jack
     				System.out.println("\nEnter First Name: ");
     				c1.setCustomerFName(scin.nextLine());
@@ -80,10 +97,50 @@ public class FoodOrdering {
     				//Same as Restaurant code -Jack
     		
     				insertCustomer(c1, connection); //Call the method that inserts the data -Jack
+    				break;
+    			case 4:
+    				customerData(connection);
+    				break;
+    			case 5:
+    				System.out.println("\nEnter a zipcode to search for restaurants in that zip code: "); 
+    				String searchZip = scin.nextLine();
+    				rZipSearch(searchZip, connection);
+    				break;
+    			default:
+    				break;
+    			
+    			}
+    			/*
+    			if(userIn.equals("Restaurant Enter") || userIn.equals("R E")) //checking if they input Restaurant or R for shorthand testing -Jack
+    			{
+    				Restaurant r1 = new Restaurant(); //create a new Restaurant object that the user will enter information into, will eventually be sent to database -Jack
+    				System.out.println("\nEnter Restaurant Name: "); //very repetitive, will be done better later, all of these will just get the information to fill out Restaurant -Jack
+    				r1.setRestaurantName(scin.nextLine()); //set the RestaurantName of our temporary Restaurant to what they enter, could be error checked later -Jack
+    				System.out.println("\nEnter Restaurant City: "); 
+    				r1.setRestaurantCity(scin.nextLine()); 
+    				System.out.println("\nEnter Restaurant State: "); 
+    				r1.setRestaurantState(scin.nextLine());
+    				System.out.println("\nEnter Restaurant Street: "); 
+    				r1.setRestaurantStreet(scin.nextLine());
+    				System.out.println("\nEnter Restaurant Zip: "); 
+    				r1.setRestaurantZip(scin.nextLine());
+    				System.out.println("\nEnter Restaurant Type: "); 
+    				r1.setRestaurantType(scin.nextLine());
+    				
+    				insertRestaurant(r1, connection); //Call the method that inserts the data -Jack
+    			}
+    			else if(userIn.equals("Customer Enter") || userIn.equals("C E"))
+    			{
+    				
     			}
     			else if(userIn.equals("Restaurant See") || userIn.equals("R S"))
     			{
-    				restaurantData(connection); //Call the method that shows the data -Ahsan
+    				showRestaurants(connection); //Call the method that shows the data -Ahsan
+    				System.out.println("Enter the nummber of the restaurant you want to order from: ");
+    				Scanner selectR = new Scanner(System.in); //Scanner is used to get data from the user -Jack
+    		    	int rSelectIn=0;
+    		    	rSelectIn=selectR.nextInt();
+    		    	selectRestaurant(connection, rSelectIn);
     			}
     			else if(userIn.equals("Customer See") || userIn.equals("C S"))
     			{
@@ -96,7 +153,7 @@ public class FoodOrdering {
     				String searchZip = scin.nextLine();
     				rZipSearch(searchZip, connection); //call the method to search for restaurants in a certain zip code -Jack
     			}
-    	
+    	*/
     		}
     	}
     	catch (SQLException e) 
@@ -125,7 +182,7 @@ public class FoodOrdering {
 		}
     	
     }
-    public static void restaurantData(Connection connection)
+    public static void showRestaurants(Connection connection)
     {
     	System.out.print("Displaying restaurant data: \n");
     	String sql = "SELECT * FROM Restaurant"; // will get all data from restaurant - ahsan
@@ -136,13 +193,14 @@ public class FoodOrdering {
 			rs = statement.executeQuery(sql); //execute sql statement
 			while(rs.next())
 			{
-				String s = rs.getString("RestaurantName")+ ": "+rs.getString("City")+", "+rs.getString("Street")+", "+rs.getString("State")+", "+rs.getString("ZipCode")+", "+rs.getString("RestaurantType")+", "+rs.getString("RestaurantRating");
+				String s = "[" + rs.getString("RestaurantID")+ "] " + rs.getString("RestaurantName")+ ": "+rs.getString("City")+", "+rs.getString("Street")+", "+rs.getString("State")+", "+rs.getString("ZipCode")+", "+rs.getString("RestaurantType")+", "+rs.getString("RestaurantRating");
 				System.out.println(s);
 				//The above displays the information and breaks the rows into a ResultSet which we then pull the data from to display - Ahsan
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}	
+		}
+		
     } 
     public static void insertRestaurant(Restaurant r1, Connection connection)
     {
@@ -190,5 +248,22 @@ public class FoodOrdering {
 			e.printStackTrace();
 		} //this isn't used yet but is how SQL statements will be inputed -Jack
 		
+    }
+    public static void selectRestaurant(Connection connection, int rSelect)
+    {
+    	String sql="SELECT * FROM [dbo].[Restaurant] WHERE RestaurantID="+rSelect+";"; //code to get the restaurant based on the ID the customer selected, will need to have the menu added -Jack
+		Statement statement;
+		try {
+			ResultSet rs;
+			statement = connection.createStatement();
+			rs = statement.executeQuery(sql); //execute sql statement
+			while(rs.next())
+			{
+				String s = rs.getString("RestaurantName")+ ": "+rs.getString("City")+", "+rs.getString("Street")+", "+rs.getString("State")+", "+rs.getString("ZipCode")+", "+rs.getString("RestaurantType")+", "+rs.getString("RestaurantRating");
+				System.out.println(s);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
     }
 }
