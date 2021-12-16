48e1eb45e5d126009a727db567eef08a9e546bb0
georj1@farmingdale.edu <jackg@DESKTOP-NJ7L5JK.myfiosgateway.com>
2021-12-16 01:29:01 -0500

    Removed unused libraries
    There were some libraries I was using for testing, they have been
    removed -Jack

--- foodOrdering/src/foodordering/FoodOrdering.java
+++ foodOrdering/src/foodordering/FoodOrdering.java
@@ -11,12 +11,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Locale;
 import java.util.Scanner;
 /**
  * @author Jack
